3b0408902dc0d5c16fef915c25b9539810a762ae
Mahadi Hassan <itsmahadi@gmail.com>
2023-01-07 23:31:14 +0600

    updating

---
+++ docker/local/postgres/maintance/_sourced/constants.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# This script is used to build the docker image for the
+BACKUP_DIR_PATH='/backups'
+BACKUP_FILE_PREFIX='backup'
---
+++ docker/local/postgres/maintance/_sourced/countdown.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+countdown() {
+  declare desc="A Simple countdown. Source: https://superuser.com/a/611582"
+  local seconds="${1}"
+  local d=$(($(date +%s) + "${seconds}"))
+
+  while [ "$d" -ge $(date +%s) ]; do
+    echo -ne "$(date -u --date @$(($d - $(date +%s))) +%H:%M:%S)\r"
+    sleep 0.1
+
+  done
+  echo
+}
